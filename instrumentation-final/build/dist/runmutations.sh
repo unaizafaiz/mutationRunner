5affe341c986f358f7f869d1231aa5526cda6835
Manaswi Karra <lkarra2@uic.edu>
2017-11-13 18:00:10 -0600

    Upto date version

--- instrumentation-final/build/dist/runmutations.sh
+++ instrumentation-final/build/dist/runmutations.sh
@@ -3,9 +3,7 @@ FILE=`which $0`
 DIST=`dirname $FILE`
 MUTATIONS_JAR=$DIST/mutations.jar
 RUNNER_JAR=$DIST/mutationrunner.jar
-HW2_OUTPUT_JAR="/Users/manaswikarra/Projects/HW2_Output/out/artifacts/HW2_Output_jar/HW2_Output.jar"
+HW2_OUTPUT_JAR="/Users/manaswikarra/Documents/OOLE/cs474_2017_hw3/lakshmimanaswi_karra_unaiza_faiz_pavan_bharadwaj_hw3/HW2_Output/build/libs/HW2_Output.jar"
 
-echo MUTATIONS_JAR=$MUTATIONS_JAR
-echo RUNNER_JAR=$RUNNER_JAR
+java -jar $RUNNER_JAR mutations.config $MUTATIONS_JAR -cp $HW2_OUTPUT_JAR sudoku.TestRunner
 
-java -jar $RUNNER_JAR mutations.config $RUNNER_JAR -cp $HW2_OUTPUT_JAR sudoku.TestRunner
